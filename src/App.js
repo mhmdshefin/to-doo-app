eaf8598530425cd04489ab9b3e4b148d531ffaa7
mhmd shefin <mshefin2005@gmail.com>
2024-08-10 22:29:19 +0530

    to doo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Addtodo from './components/Addtodo';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchTodo from './components/SearchTodo';
+import ViewallTodo from './components/ViewallTodo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Addtodo/>}/>
+      <Route path='/search' element={<SearchTodo/>}/>
+      <Route path='/viewall' element={<ViewallTodo/>}/>
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/components/Addtodo.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Addtodo = () => {
+  return (
+    <div>
+      <Navbar/>
+      <br />
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">User ID</label>
+                        <input type="text" className="form-control" placeholder='enter user id'/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">ID</label>
+                        <input type="text" className="form-control" placeholder='enter ID' />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Title</label>
+                    <input type="text" className="form-control" placeholder='enter title' />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                      <label htmlFor="" className="form-label">Status</label>
+                  <select name="" id="" className="form-select">
+                    <option value="select status">Select status</option>
+                    <option value="complete">complete</option>
+                    <option value="incomplete">incomplete</option>
+                  </select>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <button className="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Addtodo
